98eb290da63055dd16618ee152d549f672cb9e28
bikashroy120 <bikashroydt@gmail.com>
2023-09-30 19:28:25 +0600

    add

--- app/utils/Heading.tsx
+++ app/utils/Heading.tsx
@@ -7,4 +7,16 @@ interface HeadProps {
 }
 
 
-const Heading
+const Heading:FC<HeadProps>  = ({title,description,keywords})=>{
+    return(
+        <>
+            <title>{title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="description" content="A brief description of the page"/>
+            <meta name="keywords" content="keyword1, keyword2, ..."/>
+        </>
+    )
+}
+
+
+export default Heading

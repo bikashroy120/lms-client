6e1b2b5846d76c2ed5b695d11e62aacd1c672074
bikashroy120 <bikashroydt@gmail.com>
2023-11-27 01:24:48 +0600

    add

--- app/admin/course/all/page.tsx
+++ app/admin/course/all/page.tsx
@@ -1,12 +1,54 @@
+"use client"
+
+
 import AdminLayout from "@/app/components/layout/AdminLayout";
+import Table from "@/app/utils/Table";
 import React from "react";
 
 type Props = {};
 
 const Page = (props: Props) => {
+
+  const data = [
+    {
+      name:"bikash",
+      roll:120,
+      age:20
+    },
+    {
+      name:"bikash2",
+      roll:120,
+      age:20
+    },
+    {
+      name:"bikash3",
+      roll:120,
+      age:20
+    },
+  ]
+
+
+  const columns = [
+    {
+      name:"Name",
+      selector: (row:any) => row.name,
+    },
+    {
+      name:"Roll",
+      selector: (row:any) => row.roll,
+    },
+    {
+      name:"Age",
+      selector: (row:any) => row.age,
+    },
+  ]
+
   return (
     <AdminLayout>
-      <div>all </div>
+        <div>
+          <Table columns={columns} data={data}/>
+        
+        </div>
     </AdminLayout>
   );
 };
---
+++ app/utils/Table.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+
+import React from 'react'
+import DataTable, { createTheme } from 'react-data-table-component';
+
+type Props = {
+    columns:any;
+    data:any;
+}
+
+const Table = ({columns,data}: Props) => {
+
+    createTheme('solarized', {
+        text: {
+          primary: '#ffffff',
+          secondary: '#fff',
+        },
+        background: {
+          default: 'transparent',
+        },
+        context: {
+          background: '#cb4b16'
+        },
+        divider: {
+          default: '#808191',
+          height:"1px"
+        },
+        action: {
+          button: 'rgba(0,0,0,.54)',
+          hover: 'rgba(0,0,0,.08)',
+          disabled: 'rgba(0,0,0,.12)',
+        },
+      }, 'dark');
+
+      const customStyles = {
+        rows: {
+            style: {
+                minHeight: '52px', // override the row height
+            },
+        },
+
+        headRows:{
+            style: {
+                minHeight: '52px', // override the row height
+                background:"red",
+                borderRadius:"20px"
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '20px', // override the cell padding for head cells
+                paddingRight: '20px',
+                fontSize:"20px",
+                background:"red",
+                borderRadius:"20px"
+            },
+        },
+        cells: {
+            style: {
+                padding:"10px 20px ",
+                paddingLeft: '20px', // override the cell padding for data cells
+                paddingRight: '20px',
+                fontSize:"18px",
+                
+            },
+        },
+    };
+  return (
+    <div className=' text-black  font-Poppins'>
+        <DataTable
+            columns={columns}
+            data={data && data}
+            customStyles={customStyles}
+        />
+    </div>
+  )
+}
+
+export default Table

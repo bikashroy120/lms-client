0108e2b3cb80d82567421ec2cfef4c0784a49bee
bikashroy120 <bikashroydt@gmail.com>
2024-01-06 09:58:16 +0600

    video plyar add

--- app/components/CourseAccess/CourseAccess.tsx
+++ app/components/CourseAccess/CourseAccess.tsx
@@ -24,9 +24,9 @@ const CourseAccess = ({id}: Props) => {
       ) : (
         <>
           <Container>
-          <div className="flex items-start lg:py-[80px] flex-col lg:flex-row py-5 justify-between gap-6">
+          <div className="flex items-start lg:py-[30px] flex-col lg:flex-row py-5 justify-between gap-6">
               <div className=" lg:w-[66%] w-full flex flex-col gap-7">
-                  <CourseAccessMedia courseData={courseData} />
+                  <CourseAccessMedia courseData={courseData} activeVideo={activeVideo} setActiveVideo={setActiveVideo}/>
               </div>
               <div className=" lg:w-[34%] w-full sticky top-[370px]">
                 <div className=" w-full flex flex-col gap-7">
--- app/components/CourseAccess/CourseAccessMedia.tsx
+++ app/components/CourseAccess/CourseAccessMedia.tsx
@@ -1,18 +1,55 @@
-import React from 'react'
+"use client";
+
+import VideoPlayer from "@/app/utils/VideoPlayer";
+import React from "react";
 
 type Props = {
-    courseData:any
-}
+  courseData: any;
+  activeVideo: number;
+  setActiveVideo: (activeVideo: number) => void;
+};
 
-const CourseAccessMedia = ({courseData}: Props) => {
-
-    
+const CourseAccessMedia = ({
+  courseData,
+  activeVideo,
+  setActiveVideo,
+}: Props) => {
+  console.log(courseData);
 
   return (
     <div>
-
+      <div className=" w-full lg:h-[570px] h-[280px] bg-black rounded-lg">
+        {/* <VideoPlayer videoUrl={courseData[activeVideo]?.videoUrl}/> */}
+      </div>
+      <div>
+        <div className=" flex items-center justify-between mt-5">
+          <button
+            onClick={() => setActiveVideo(activeVideo - 1)}
+            disabled={activeVideo === 0}
+            className={`bg-green-500 text-white font-semibold text-[18px] py-2 px-5 rounded-lg ${
+              activeVideo === 0
+              ? " cursor-not-allowed bg-gray-400 text-gray-300"
+              : " cursor-pointer bg-green-500 text-white"
+            }`}
+          >
+            Prev Lesson
+          </button>
+          <button
+            onClick={() => setActiveVideo(activeVideo + 1)}
+            disabled={courseData.length-1 === activeVideo}
+            className={` font-semibold text-[18px] py-2 px-5 rounded-lg ${
+              courseData.length-1 === activeVideo
+                ? " cursor-not-allowed bg-gray-400 text-gray-300"
+                : " cursor-pointer bg-green-500 text-white"
+            }`}
+          >
+            Next Lesson
+          </button>
+        </div>
+        <h2>{courseData[activeVideo]?.title}</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseAccessMedia
+export default CourseAccessMedia;

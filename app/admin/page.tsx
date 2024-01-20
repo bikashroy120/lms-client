73b179393d2a556b01a043e790be4749bf1a5cb7
bikashroy120 <bikashroydt@gmail.com>
2024-01-20 08:30:35 +0600

    admin card add

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import AdminLayout from "../components/layout/AdminLayout";
 import AdminProtected from "../components/hooks/useAdminProtected";
 import AdminCard from "../components/admin/admin/AdminCard";
+import { FaBookOpenReader, FaDollarSign } from "react-icons/fa6";
+import { PiStudentBold } from "react-icons/pi";
 
 type Props = {};
 
@@ -14,22 +16,25 @@ const Page = (props: Props) => {
 
   const carValue = [
     {
-      title:"",
-      value:"",
-      icon:"",
-      className:"",
+      title:"Total Revenue",
+      value:"$500",
+      icon:<FaDollarSign />,
+      className:"border-green-500 border-b-[5px] border-b-green-500",
+      bg:"bg-green-500"
     },
     {
-      title:"",
-      value:"",
-      icon:"",
-      className:"",
+      title:"Total Enroll",
+      value:"200",
+      icon:<FaBookOpenReader />,
+      className:"border-primary border-b-[5px] border-b-primary",
+      bg:"bg-primary"
     },
     {
-      title:"",
-      value:"",
-      icon:"",
-      className:"",
+      title:"Total Students",
+      value:"100",
+      icon:<PiStudentBold />,
+      className:"border-success border-b-[5px] border-b-success",
+      bg:"bg-success"
     }
   ]
 
@@ -38,10 +43,13 @@ const Page = (props: Props) => {
     <AdminLayout>
       {/* <AdminProtected> */}
         <div>
-            <div className=" grid grid-cols-1 md:grid-cols-3 gap-7">
+            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
               {carValue.map((item:any,index:number)=>(
                 <AdminCard data={item} key={index}/>
               ))}
+            </div>
+            <div>
+                
             </div>
         </div>
       {/* </AdminProtected> */}
--- app/components/admin/admin/AdminCard.tsx
+++ app/components/admin/admin/AdminCard.tsx
@@ -6,9 +6,13 @@ type Props = {
 
 const AdminCard = ({data}: Props) => {
   return (
-    <div className={`py-5 px-5 rounded-xl border border-primary`}>
+    <div className={`py-5 px-5 flex bg-white items-center justify-between shadow-md rounded-xl border ${data?.className}`}>
         <div>
-          <h2>{data?.title}</h2>
+          <h2 className=' text-text text-[20px] font-semibold'>{data?.title}</h2>
+          <h2 className='text-text text-[30px] font-bold'>{data?.value}</h2>
+        </div>
+        <div className={`text-[35px] flex items-center justify-center shadow-md w-[60px] h-[60px] rounded-md text-white ${data?.bg}`}>
+            {data?.icon}
         </div>
     </div>
   )
